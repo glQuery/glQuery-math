063617df833bc948f395c6a8f801231661f75bfc
Rehno Lindeque <errantkid@gmail.com>
2012-01-02 21:12:47 +0200

    Extend glquery with the math module if it is visible Added a separate file for the copyright header

---
+++ src/header.js
@@ -0,0 +1,6 @@
+/*
+ * glQuery-math - A math module from a fluent WebGL engine (https://github.com/glQuery)
+ * glQuery-math is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
+ * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
+ */
+
--- src/wrapped.footer.js
+++ src/wrapped.footer.js
@@ -1,3 +1,9 @@
+
+  // Extend glQuery if it is defined
+  if (glQuery != null)
+    for(key in this)
+      if (glQuery[key] == null)
+        glQuery[key] = this[key];
   return this;
 })();
 

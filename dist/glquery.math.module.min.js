96c100062577b35995134c501877dc24c6b6f848
Rehno Lindeque <errantkid@gmail.com>
2012-01-02 22:28:22 +0200

    Add the non-copyright header to the module files

--- dist/glquery.math.module.min.js
+++ dist/glquery.math.module.min.js
@@ -1,3 +1,11 @@
+/*
+ * glQuery-math - A math module from a fluent WebGL engine (https://github.com/glQuery)
+ * glQuery-math is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
+ * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
+ */
+
+
+
 var glQueryMath = (function() {
 "use strict";
 

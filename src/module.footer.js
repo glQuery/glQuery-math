e62c7fc217c60b3a8376a90ef5543cf91c6b3608
Rehno Lindeque <errantkid@gmail.com>
2012-01-13 18:09:57 -0800

    Fixed the test for extending glQuery when glQuery is undefined (reference exception)

--- src/module.footer.js
+++ src/module.footer.js
@@ -1,6 +1,6 @@
 
 // Extend glQuery if it is defined
-if (glQuery != null)
+if (typeof glQuery !== 'undefined' && glQuery != null)
   for(var key in glQueryMath)
     if (glQuery[key] == null)
       glQuery[key] = glQueryMath[key];
